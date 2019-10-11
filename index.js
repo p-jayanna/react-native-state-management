23e6896f4fd2d21ae3688e3f44d21d942b2383dc
Jayanna Praveenkumar <pj80930@wr2pj80930g5m.fritz.box>
2019-10-11 19:46:31 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,21 @@
+/**
+ * @format
+ */
+import React from 'react';
+
+import {AppRegistry} from 'react-native';
+import {Provider} from 'react-redux';
+
+import configureStore from './configureStore';
+import App from './App';
+import {name as appName} from './app.json';
+
+const store = configureStore();
+
+const ReduxApp = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+AppRegistry.registerComponent(appName, () => ReduxApp);
